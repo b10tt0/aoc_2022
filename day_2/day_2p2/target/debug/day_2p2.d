a0f41728374a1cd9eb1fba4828d6f2a7541cd9f5
b10tt0 <b4b0000000@gmail.com>
2022-12-04 17:47:22 -0500

    removing superfluous files

--- day_2/day_2p2/target/debug/day_2p2.d
+++
@@ -1,1 +0,0 @@
-/home/b10tt0/aoc_2022/day_2/day_2p2/target/debug/day_2p2: /home/b10tt0/aoc_2022/day_2/day_2p2/src/main.rs
--- day_2/day_2p2/target/debug/deps/day_2p2-b6010e2f2e1d63eb.d
+++
@@ -1,5 +0,0 @@
-/home/b10tt0/aoc_2022/day_2/day_2p2/target/debug/deps/day_2p2-b6010e2f2e1d63eb: src/main.rs
-
-/home/b10tt0/aoc_2022/day_2/day_2p2/target/debug/deps/day_2p2-b6010e2f2e1d63eb.d: src/main.rs
-
-src/main.rs:
